84851225043086930774e7188efa7582636e8895
Dmitry Warkentin <web.warkentin@gmail.com>
2020-02-06 22:04:14 +0400

    'init'

---
+++ README.md
@@ -0,0 +1,108 @@
+<p align="center">
+  <img src="https://res.cloudinary.com/wark/image/upload/v1576917345/instanon.png" width="638px">
+  <h2 align="center" style="margin-top: -4px !important;">Download Instagram stories or highlights anonymously</h2>
+  <p align="center">
+    <a href="https://github.com/dt-wark/instanon/blob/master/LICENSE">
+      <img src="https://img.shields.io/badge/license-MIT-green.svg">
+    </a>
+    <a href="https://www.python.org/">
+    	<img src="https://img.shields.io/badge/python-v3.8-blue.svg">
+    </a>
+    <a href="#">
+      <img src="https://img.shields.io/badge/status-stable-brightgreen.svg">
+    </a>
+    <a href="https://github.com/SeleniumHQ/selenium">
+      <img src="https://img.shields.io/badge/built%20with-Selenium-yellow.svg">
+    </a>
+  <a href="https://github.com/dt-wark/instanon/pulls?utf8=%E2%9C%93&amp;q=is%3Apr%20author%3Aapp%2Fdependabot"><img src="https://camo.githubusercontent.com/6266857d1c53194119edf1d9aafae7a4b301fa16/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f646570656e64656e636965732d7570253230746f253230646174652d627269676874677265656e2e737667" alt="Dependencies Status" data-canonical-src="https://img.shields.io/badge/dependencies-up%20to%20date-brightgreen.svg" style="max-width:100%;"></a>
+    <a href="https://res.cloudinary.com/wark/image/upload/v1576597812/bit.png">
+      <img src="https://img.shields.io/badge/btc-1AnYqP7mt7QxqYc6fmQk5m6YHN8Rqan4ze-informational.svg">
+    </a>
+  </p>
+</p>
+
+
+## Setup
+
+1. Install Python 3.8
+2. Clone this repo
+```
+git clone https://github.com/dt-wark/instanon.git
+```
+
+3. Create and activate virtual environment
+```
+python3.8 -m venv venv
+source venv/bin/activate
+```
+
+4. Install dependencies
+```
+pip3 install -r requirements.txt
+```
+
+## Usage
+
+```
+Options:
+  -u, --users        Instagram username(s)
+  -s, --stories      Download stories
+  -h, --highlights   Download highlights
+  -o, --output       Directory for data storage
+  -c, --chaos        Save stories in one directory
+```
+
+### Example:
+
+#### Download stories and save them to /mnt/e/instagram directory
+```
+python3.8 instanon.py -u katie_westwood -u sharishanya --stories --chaos --output "/mnt/e/instagram"
+```
+
+#### Download highlights and save them to /mnt/e/instagram directory
+```
+python3.8 instanon.py -u katie_westwood --highlights --output "/mnt/e/instagram"
+```
+
+#### Download stories and save them to instanon script directory with script_run_days directories 
+```
+python3.8 instanon.py -u katie_westwood --stories
+```
+
+
+## Tasks Automation
+
+### Linux/osx 
+
+#### cron
+
+```
+# run the script every day at 1 p.m.
+0 13 * * * root source /path/to/virtualenv/bin/activate && python3.8 /path/to/instanon/instanon.py -u USER --stories
+```
+
+### Windows
+
+#### Windows Task Scheduler
+
+- Press Win+R → type taskschd.msc
+
+- In the Task Scheduler window, go to the Task Scheduler Library row, go to the Actions column on the right. There, click "Create Task" link, to open the wizard bearing the same name.
+
+- Enter a name and a description for your new task. Example:
+<img src="https://res.cloudinary.com/wark/image/upload/v1576572206/task-name.png" style="margin-top: 12px !important;">
+
+- Also select "Run whether user is logged on or not", choose HIDDEN option and сhoose the configuration for Windows 10.
+
+- In the Triggers tab, click New... and set the script runtime.
+
+- In the Actions tab, enter your Python path, script options and script folder path. Example:
+<img src="https://res.cloudinary.com/wark/image/upload/v1577606322/actions.png" style="margin-top: 12px !important;">
+
+<hr>
+
+#### You can also create a batch script on your desktop and run it if necessary.
+```
+@echo off
+cmd /c "E:\path\to\venv\Scripts\python.exe E:\path\to\instanon.py -u USER1 -u USER2 -s -o "E:\your\path""
+```
---
+++ instanon.py
@@ -0,0 +1,363 @@
+import os
+import re
+import shutil
+
+from datetime import datetime
+from html import unescape
+from sys import exit
+from urllib.parse import urlparse
+
+import click
+import requests
+import urllib3
+
+from bs4 import BeautifulSoup
+from urllib3.exceptions import InsecureRequestWarning
+
+
+class Directories:
+    """Contains path information and directory creation function.
+
+    Arg:
+        username (str): Instagram username
+
+    Optional args:
+        output_flag (str): Custom output path
+        chaos_flag (bool): Chaos option (all stories in one directory)
+
+    Methods:
+        create() -> Creates the specified directories
+        rm_empty_stories_dirs() -> Removes empty dirs from the story dir
+    """
+
+    def __init__(self, username, output_flag=False, chaos_flag=False):
+        """
+        Params:
+            username (str): Instagram username
+            root_path (str): Root directory path with usernames
+            highlights_path (str): Highlights directory path
+            stories_path (str): Stories directory path
+        """
+
+        self.username = username
+
+        # If custom output path is passed
+        # set root_path with this directory
+        if output_flag:
+            self.root_path = r'{}'.format(output_flag)
+        else:
+            self.root_path = 'users'
+
+        self.user_path = os.path.join(self.root_path, self.username)
+        self.highlights_path = os.path.join(self.user_path, 'highlights')
+
+        # If chaos_flag is not passed
+        # add a directory with today's date
+        if chaos_flag:
+            self.stories_path = os.path.join(self.user_path, 'stories')
+        else:
+            self.stories_path = os.path.join(self.user_path, 'stories',
+                                             datetime.now().strftime("%d-%B-%Y"))
+
+    def create(self, stories_flag=False, highlights_flag=False):
+        """Creates the necessary directories.
+
+        Optional args:
+            stories_flag (bool): Create stories directory
+            highlights_flag (bool): Create highlights directory
+        """
+
+        def create_dir(dir_path):
+            if not os.path.isdir(dir_path):
+                os.makedirs(dir_path)
+
+        # Create main directories
+        create_dir(self.root_path)
+        create_dir(self.user_path)
+
+        # Create optional directories
+        if stories_flag:
+            create_dir(self.stories_path)
+        if highlights_flag:
+            create_dir(self.highlights_path)
+
+    def rm_empty_stories_dirs(self):
+        """Removes empty dirs from the story dir"""
+
+        try:
+            if os.listdir(os.path.abspath(self.stories_path)) == []:
+                shutil.rmtree(os.path.abspath(self.stories_path))
+        except FileNotFoundError:
+            pass
+
+
+class Content(Directories):
+    """Collects user data.
+
+    Arg:
+        username (str): Instagram username
+    Optional args:
+        output_flag (str): Сustom output path
+        chaos_flag (bool): Сhaos option (all stories in one directory)
+    Methods:
+        exists() -> Checks the user's profile
+        get_stories() -> Get user stories
+        download_stories() -> Download user stories
+        get_highlights() -> Get user highlights
+        download_highlights() -> Download user highlights
+        get_all_filenames_in_dir() -> Get all file names in dir
+        validate() -> Check the passed file name in user_path
+    """
+
+    def __init__(self, username, output_flag=False, chaos_flag=False):
+        """
+        Params:
+            username (str): Instagram username
+            api (str): Main site for parsing
+            stories_link (str): Link to user stories
+            user_link (str): Link to user content page
+            root_page (str): HTML page with user content
+
+            Inherited paths from the Directories class
+        """
+
+        self.username = username
+        self.api = 'https://storiesig.com'
+        self.stories_link = self.api + '/stories/' + self.username
+        self.user_link = self.api + '?username=' + self.username
+        self.root_page = requests.get(self.user_link, verify=False).text
+        super().__init__(username, output_flag=output_flag, chaos_flag=chaos_flag)
+
+    def exists(self):
+        """Checks the user's profile.
+
+        Return
+            True: If the user exists and their profile is open
+            False: If not
+        """
+
+        if "Sorry, this username isn't available." in self.root_page:
+            instagram_page = f"https://www.instagram.com/{self.username}"
+            if requests.get(instagram_page).status_code == 404:
+                click.secho(f"[!] User '{self.username}' does not exist", fg='red')
+            else:
+                click.secho(f"[!] Server error. Please try again later", fg='red')
+            return False
+        elif "This Account is Private" in self.root_page:
+            click.secho(f"[!] Account '{self.username}' is private", fg='yellow')
+            return False
+        else:
+            return True
+
+    def get_stories(self):
+        """Gets user stories.
+
+        Return
+            stories (list): Stories links
+        """
+
+        r = requests.get(self.stories_link, verify=False).text
+        if 'No stories to show' in r:
+            click.secho(f"\n[!] Whoops! {self.username} did not post "
+                        "any recent stories", fg='yellow')
+            return False
+        else:
+            print(f"\n[*] Getting {self.username} stories")
+            stories = []
+            soup = BeautifulSoup(r, features="lxml")
+            links = soup.findAll('a', attrs={'href': re.compile("^https://scontent")})
+
+            for link in links:
+                url = link.get('href')
+                stories.append(unescape(url))
+            return stories
+
+    def download_stories(self, stories_pool):
+        """Downloads user stories.
+
+        Arg
+            stories_pool (list): Stories links
+        """
+
+        try:
+            fill_char = click.style('=', fg='green')
+            with click.progressbar(stories_pool,
+                                   label='[*] Downloading stories',
+                                   fill_char=fill_char) as bar:
+
+                # For every link in stories_pool
+                for link in bar:
+                    r = requests.get(link, verify=False)
+                    parser = urlparse(link)
+                    filename = os.path.basename(parser.path)
+
+                    # Validate and dowload stories
+                    if self.validate(filename):
+                        with open(os.path.join(self.stories_path, filename), 'wb') as f:
+                            f.write(r.content)
+                    else:
+                        continue
+        except KeyboardInterrupt:
+            exit()
+
+    def get_highlights(self):
+        """Gets user highlights.
+
+        Return
+            highlights_dictionary (dict): {link: dirname} of each highlights group
+        """
+
+        hlarray = []    # Links to every highlights group
+        hlnarray = []   # Last parts of the links ↑
+        hnarray = []    # Highlights names
+        hdirname = []   # Highlights directory names
+
+        # Parsing URLs
+        soup = BeautifulSoup(self.root_page, features="lxml")
+        hlinks = soup.findAll('a', attrs={'href': re.compile("^/highlights/")})
+        if hlinks:
+            print(f"\n[*] Getting {self.username} highlights")
+
+            # Get links to every highlights group and last parts of this links
+            for highlight in hlinks:
+                url = highlight.get('href')
+                parser = urlparse(url)
+                hname = os.path.basename(parser.path)
+                hlnarray.append(hname)
+                hlarray.append(self.api + url)
+
+            # Get highlights names
+            hnames = soup.findAll("img", {"class": "jsx-2521016335"})
+            for i in hnames:
+                dname = i['alt']
+                hnarray.append(dname)
+
+            # Get highlights directory names
+            for i, j in zip(hnarray, hlnarray):
+                hdirname.append(i + '_' + j)
+
+            # Get {link: dirname} of each highlights group
+            highlights_dictionary = dict(zip(hlarray, hdirname))
+            return highlights_dictionary
+        else:
+            click.secho(f"\n[!] Whoops! {self.username} does not appear "
+                        "to have any highlights", fg='yellow')
+            return False
+
+    def download_highlight(self, key, value, start, end):
+        """Downloads user stories.
+
+        Args
+            key (str): Link to every highlights group
+            value (str): Highlights directory name
+            start (int): Start counter highlights groups
+            end (int): End counter highlights groups
+        """
+
+        # Get highlights group page
+        html = requests.get(key, verify=False).text
+
+        # Create a directory
+        od = os.path.join(self.highlights_path, value)
+        if not os.path.isdir(od):
+            os.makedirs(od)
+
+        # Parsing all highlights of the group
+        soup = BeautifulSoup(html, features="lxml")
+        links = soup.findAll('a', attrs={'href': re.compile("^https://scontent")})
+
+        # Download every highlight
+        try:
+            bar_label = f'[*] Downloading highlight {start} of {end}'
+            fill_char = click.style('=', fg='green')
+            with click.progressbar(links,
+                                   label=bar_label,
+                                   fill_char=fill_char) as bar:
+                for link in bar:
+                    url = link.get('href')
+                    r = requests.get(url, verify=False)
+                    parser = urlparse(url)
+                    filename = os.path.basename(parser.path)
+                    if self.validate(filename):
+                        with open(os.path.join(od, filename), 'wb') as f:
+                            f.write(r.content)
+                    else:
+                        continue
+        except KeyboardInterrupt:
+            exit()
+
+    def get_all_filenames_in_dir(self, dir_path):
+        """Gets all file names in the passed directory.
+
+        Arg
+            dir_path (str): User directory path
+        Return
+            all_filenames (list): List of file names
+        """
+
+        all_filenames = []
+        for root, directories, files in os.walk(dir_path):
+            for filename in files:
+                if not filename.startswith('.'):
+                    all_filenames.append(filename)
+        return all_filenames
+
+    def validate(self, user_file):
+        """Check the passed file name in user_path.
+
+        Arg
+            user_file (str): file name
+        Return
+            True or False
+        """
+
+        user_data = self.get_all_filenames_in_dir(self.user_path)
+        if user_file in user_data:
+            return False
+        else:
+            return True
+
+
+@click.command()
+@click.option('--users', '-u', type=str, multiple=True, required=True, help='Instagram username(s)')
+@click.option('--stories', '-s', type=str, is_flag=True, help='Download stories')
+@click.option('--highlights', '-h', type=str, is_flag=True, help='Download highlights')
+@click.option('--output', '-o', type=click.Path(), help='Directory for data storage')
+@click.option('--chaos', '-c', type=str, is_flag=True, help='Save stories in one directory')
+def main(users, stories, highlights, output, chaos):
+    """Download Instagram stories or highlights anonymously"""
+
+    # disable certificate error
+    urllib3.disable_warnings(InsecureRequestWarning)
+
+    for user in users:
+        user_content = Content(user, output, chaos)
+        if user_content.exists():
+            dirs = Directories(user, output, chaos)
+
+            if stories:
+                stories_pool = user_content.get_stories()
+                if stories_pool:
+                    dirs.create(stories_flag=True)
+                    user_content.download_stories(stories_pool)
+                    dirs.rm_empty_stories_dirs()
+                else:
+                    pass
+
+            if highlights:
+                highlights_pool = user_content.get_highlights()
+                if highlights_pool:
+                    dirs.create(highlights_flag=True)
+                    # init highlights group counter
+                    start, end = 1, len(highlights_pool)
+                    for key, value in highlights_pool.items():
+                        user_content.download_highlight(key, value, start, end)
+                        start += 1
+                else:
+                    pass
+
+    click.secho(f'\n[*] All tasks have been completed\n', fg='green')
+
+
+if __name__ == "__main__":
+    main()
